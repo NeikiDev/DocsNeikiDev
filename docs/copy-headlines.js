b5a164e4cfa02d3b7ca83a031d0633e7b112a955
Neiki <philippeckert.1@gmx.de>
2023-03-17 16:15:07 +0100

    test theme 1

--- docs/copy-headlines.js
+++ docs/copy-headlines.js
@@ -2,7 +2,6 @@ function LoadCopyPluginFunction() {
     const h1s = document.querySelectorAll('h1')
     const h2s = document.querySelectorAll('h2')
     const h3s = document.querySelectorAll('h3')
-    const h4s = document.querySelectorAll('h4')
 
     h1s.forEach(header => {
         header.addEventListener('click', () => {
@@ -52,28 +51,10 @@ function LoadCopyPluginFunction() {
         })
     })
 
-    h4s.forEach(header => {
-        header.addEventListener('click', () => {
-            const currentURL = window.location.href.split("?")[0].split("#")[1]
-            navigator.clipboard.writeText(`https://docs.neiki.dev/#${currentURL}?id=${header.id}`)
-            Swal.fire({
-                title: 'Copied Link to Clipboard!',
-                toast: true,
-                position: 'top',
-                icon: 'success',
-                timer: 2000,
-                timerProgressBar: true,
-                showConfirmButton: false,
-            })
-        })
-    })
-
     window.addEventListener('hashchange', () => {
         const h1s_new = document.querySelectorAll('h1')
         const h2s_new = document.querySelectorAll('h2')
         const h3s_new = document.querySelectorAll('h3')
-        const h4s_new = document.querySelectorAll('h4')
-
 
         h1s_new.forEach(header => {
             header.addEventListener('click', () => {
@@ -123,20 +104,5 @@ function LoadCopyPluginFunction() {
             })
         })
 
-        h4s_new.forEach(header => {
-            header.addEventListener('click', () => {
-                const currentURL = window.location.href.split("?")[0].split("#")[1]
-                navigator.clipboard.writeText(`https://docs.neiki.dev/#${currentURL}?id=${header.id}`)
-                Swal.fire({
-                    title: 'Copied Link to Clipboard!',
-                    toast: true,
-                    position: 'top',
-                    icon: 'success',
-                    timer: 2000,
-                    timerProgressBar: true,
-                    showConfirmButton: false,
-                })
-            })
-        })
     })
 }
